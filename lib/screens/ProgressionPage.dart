3da827e9692e1dc614ab2ff3e4e05f4f21f519a3
Amael Maserati <amael.maserati@etu.univ-orleans.fr>
2024-03-25 14:58:37 +0100

    front de la page des niveaux faite

---
+++ lib/screens/ProgressionPage.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class ProgressionPage extends StatelessWidget {
+  final int totalNiveaux;
+  final int niveauxCompletes;
+
+  ProgressionPage({required this.totalNiveaux, required this.niveauxCompletes});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Aventure'),
+      ),
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            children: List.generate(totalNiveaux * 2 - 1, (index) {
+              if (index % 2 == 0) {
+                int niveau = totalNiveaux - index ~/ 2;
+                return CircleAvatar(
+                  backgroundColor: niveau <= niveauxCompletes ? Colors.green : Colors.grey,
+                  child: Text(niveau.toString()),
+                );
+              } else {
+                return SizedBox(
+                  height: 20,
+                  child: VerticalDivider(
+                    color: Colors.black,
+                    thickness: 2,
+                  ),
+                );
+              }
+            }),
+          ),
+        ),
+      ),
+    );
+  }
+}
