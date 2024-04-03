19ce7a64e9f529b80f9bd82e708f9224f2a86ca3
QuentinDroucheau <quentin.droucheau@etu.univ-orleans.fr>
2024-04-03 09:40:05 +0200

    suppression du debug

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,7 @@ class MysteryNumberApp extends StatelessWidget{
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Mystery Number',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
